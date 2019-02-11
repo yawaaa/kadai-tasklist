c530b78c80ce894bd11a4c15cabe31010b51d2e4
teppei <suteaka1606135@gmail.com>
2019-02-11 05:53:04 +0000

    make login function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  
+  
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "ユーザ登録完了"
+      redirect_to @user
+    else
+      flash[:danger] = "ユーザ登録失敗"
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,34 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  # get 'users/index'
+
+  # get 'users/show'
+
+  # get 'users/new'
+
+  # get 'users/create'
+
   # これが無いと / が/tasks へルーティングされないっぽい
   root to: "tasks#index"
   
-  # テンプレ 
-  resources :tasks
+  #  URLを/signupにするためだけ
+  get "login", to: "session#new"
+  post "login", to: "session#new"
+  delete "logout", to: "sessios#new"
 
+
+  get "signup", to: "user#new"
+
+  # テンプレ 
+  # TASK CRUD操作用
+  resources :tasks
+  # user CRUD操作用。ただしeditとかは今回は作らない。
+  resources :users, only: [:index, :show, :new, :create]
 
 end
